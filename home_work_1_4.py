2fb8221eed8910c7107487317fd91caa134e18c7
paaltukhov <pavel.ancl@gmail.com>
2021-02-01 22:02:38 +0300

    add file "home_work_1_6"

--- home_work_1_4.py
+++ home_work_1_4.py
@@ -1,12 +1,18 @@
 # Пользователь вводит целое положительное число. Найдите самую большую
 # цифру в числе. Для решения используйте цикл while и арифметические операции.
 
-number = int(input("Введите целое положительное число: "))
-max_d = number % 10
-while number > 0:
-    number //= 10
-    if max_d < number % 10:
-        max_d = number % 10
+num_init = int(input("Введите целое положительное число: "))
+max_d = 0
+num = num_init
+
+while num > 0:
+    digit = num % 10
+    if digit > max_d:
+        max_d = digit
+        if max_d == 9:
+            break
+    num = num // 10
+
 print(max_d)
 
 # Надо научиться так думать
