4a2e3c2ddf0c241af0aa49c7a3e37d767bb46ee7
paaltukhov <pavel.ancl@gmail.com>
2021-03-08 21:25:35 +0300

    add file "hw_3_6"

---
+++ hw_3_6.py
@@ -0,0 +1,31 @@
+# Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же, но с прописной
+# первой буквой. Например, print(int_func(‘text’)) -> Text.
+#
+# Продолжить работу над заданием. В программу должна попадать строка из слов, разделенных пробелом. Каждое слово
+# состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки, но каждое слово должно начинаться с
+# заглавной буквы. Необходимо использовать написанную ранее функцию int_func().
+
+
+def int_func(text):
+    return text.title()
+
+
+txt = input("введи слово: ")
+
+print(int_func(txt))
+
+
+def str_func(user_answer):
+    return " ".join(map(str.capitalize, user_answer))
+
+
+print(str_func(user_answer=input("enter words separated by a space: ").split()))
+
+
+def strr(txt):
+    txt = txt[0].upper() + txt[1:]
+    return txt
+
+
+text = input("say the word: ")
+print(strr(text))
