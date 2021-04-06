13a3035ea2f5b724e2b04018d6ad4d63aee95d4e
paaltukhov <pavel.ancl@gmail.com>
2021-04-06 22:28:00 +0300

    add file "hw_5_4"

---
+++ hw_5_4.py
@@ -0,0 +1,20 @@
+# 4. Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+# При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться
+# в новый текстовый файл.
+
+from translate import Translator
+
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_4.txt", "r", encoding="utf-8") as new_f:
+    with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_4_1.txt", "w", encoding="utf-8") as new_f_1:
+        for line in new_f.readlines():
+            line = list(line.split(" "))
+            translator = Translator(to_lang="Russian")
+            line[0] = translator.translate(line[0])
+            line = " ".join(line)
+            print(line)
+            new_f_1.writelines(line)
