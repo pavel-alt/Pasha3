bfca150e47f5450d5c6aa98d38050fbabe14c46d
paaltukhov <pavel.ancl@gmail.com>
2021-04-07 14:57:10 +0300

    add file "hw_5_5"

--- hw_5_5.py
+++ hw_5_5.py
@@ -1,4 +1,14 @@
 # 5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. Программа
 # должна подсчитывать сумму чисел в файле и выводить ее на экран.
 
+from random import randint
+
+
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "w", encoding="utf-8") as new_f:
+    new_f.writelines(' '.join([str(randint(1, 100)) for el in range(10)]))
+
 with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "r", encoding="utf-8") as new_f:
+    res = sum([int(el) for el in list(new_f.readline().split(" "))])
+
+print(res)
+
