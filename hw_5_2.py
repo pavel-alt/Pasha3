d968826e1fc1bbe9ca766c12272a4ecd7538cc3b
paaltukhov <pavel.ancl@gmail.com>
2021-04-17 13:28:14 +0300

    add file "hw_5_2"

--- hw_5_2.py
+++ hw_5_2.py
@@ -4,12 +4,12 @@
 
 """Изначальный вариант"""
 
-# with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_2.txt", "r") as new_f:
-#     print(f"The number of strings is - {len(new_f.readlines())}")
-#     new_f.seek(0)
-#     for s in new_f.readlines():
-#         w = len(s.split(" "))
-#         print(f"The number of words in string - {w}")
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_2.txt", "r") as new_f:
+    print(f"The number of strings is - {len(new_f.readlines())}")
+    new_f.seek(0)
+    for s in new_f.readlines():
+        w = len(s.split(" "))
+        print(f"The number of words in string - {w}")
 
 """Вариант преподавателя"""
 
