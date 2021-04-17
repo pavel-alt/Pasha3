168b98e027ff34a08ae3a0422b463f9f0d708fdd
paaltukhov <pavel.ancl@gmail.com>
2021-04-17 13:07:25 +0300

    add file "hw_5_3"

--- hw_5_3.py
+++ hw_5_3.py
@@ -1,7 +1,9 @@
-# 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. Определить,
-# кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины
-# дохода сотрудников.
+"""3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. Определить,
+кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины
+дохода сотрудников."""
 
+
+"""Изначальный вариант"""
 with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_3.txt", "r") as new_f:
     sum_salary = 0
     for staff in new_f.readlines():
@@ -13,3 +15,16 @@ with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_3.txt", "r") 
     average = sum_salary / len(new_f.readlines())
     print(f"Average salary in company is {average}")
 
+"""Вариант преподавателя"""
+incomes = []
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_3.txt", "r") as new_f:
+    lines = new_f.readlines()
+
+    for line in lines:
+        name, income = line.split(",")
+        income = int(income)
+        if income < 2000:
+            print(name)
+        print(sum(incomes) / len(incomes))
+
+"""Вариант преподавателя не работает, т.к. исходный текст создан иначе"""
