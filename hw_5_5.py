e3d907ff5bf9fd67fa05ba646986a67c92532576
paaltukhov <pavel.ancl@gmail.com>
2021-04-17 13:40:18 +0300

    add file "hw_5_5"

--- hw_5_5.py
+++ hw_5_5.py
@@ -1,5 +1,7 @@
-# 5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. Программа
-# должна подсчитывать сумму чисел в файле и выводить ее на экран.
+"""5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. Программа
+должна подсчитывать сумму чисел в файле и выводить ее на экран."""
+
+"""Изначальный вариант"""
 
 from random import randint
 
@@ -8,7 +10,17 @@ with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "w", 
     new_f.writelines(' '.join([str(randint(1, 100)) for el in range(10)]))
 
 with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "r", encoding="utf-8") as new_f:
-    res = sum([int(el) for el in list(new_f.readline().split(" "))])
+    # res = sum([int(el) for el in list(new_f.readline().split(" "))])
+    res = sum(map(int, list(new_f.readline().split(" "))))
 
 print(res)
 
+"""Вариант преподавателя"""
+
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "w", encoding="utf-8") as new_f:
+    num = f"{input('введите числа через пробел: ')}"
+    new_f.write(num)
+with open(r"C:\Users\paaltukhov\PycharmProjects\Паша\test_hw_5_5.txt", "r", encoding="utf-8") as new_f:
+    main_line = new_f.readline()
+    main_list = map(int, main_line.split())
+    print(sum(main_list))
