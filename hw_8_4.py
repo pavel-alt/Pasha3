61bc1f17a27fac018a8fd1ae70b3bfdba1989fa5
pavel-alt <pavel.ancl@gmail.com>
2021-07-03 00:43:37 +0300

    тест каммита

--- hw_8_4.py
+++ hw_8_4.py
@@ -7,100 +7,120 @@
 5. Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники на склад и передачу в
 определенное подразделение компании. Для хранения данных о наименовании и количестве единиц оргтехники, а также других
 данных, можно использовать любую подходящую структуру, например словарь.
-"""
 
-
-
-"""
-В класс скад добавить параметр максимальной вместительности, добавить поле наполненности на складе, сделать метод 
+В класс скад добавить параметр максимальной вместительности, добавить поле наполненности на складе, сделать метод
 добавления объектов из офисэквипмента.
+
+6. Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных. Например,
+для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
+Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей, изученных
+на уроках по ООП.
 """
+
 from abc import ABC, abstractmethod
 
 
-
-
 class Storage:
-    def __init__(self):
-        pass
-in_storage = []
+
+    list_storage = []
+
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+        self.list_object = []
+        Storage.list_storage.append(self)
+
+    def getting(self, obj):
+        if len(self.list_object) < self.capacity:
+            if issubclass(type(obj), OfficeEquipment):
+                self.list_object.append(obj)
+                print("Object in storage")
+            else:
+                print("Zobery eto")
+        else:
+            print("Storage is full")
+
+    def take_away(self, obj):
+        if obj in self.list_object:
+            self.list_object.remove(obj)
+            print("Object deleted")
+
+    @staticmethod
+    def audit(storage):
+        printer, scanner, copier = 0, 0, 0
+        for obj in storage.list_object:
+            if isinstance(obj, Printer):
+                printer += 1
+            if isinstance(obj, Scanner):
+                scanner += 1
+            if isinstance(obj, Copier):
+                copier += 1
+
+        in_storage = {'Printer': printer, 'Scanner': scanner, 'copier': copier}
+        print(in_storage)
 
 
 class OfficeEquipment(ABC):
-
-
-
-    def __init__(self, type_eq, manufacturing, model, price):
-        self.type_eq = type_eq
-        self.manufacturing = manufacturing
-        self.model = model
-        self.price = price
+    def __init__(self, firm='canon'):
+        self.firm = firm
 
     @abstractmethod
     def action(self):
         pass
 
-    def getting(self):
-        if OfficeEquipment:
-            in_warehouse[f'{self.type_eq}'] = [self.manufacturing, self.model, self.price]
-
 
 class Printer(OfficeEquipment):
-    count_printer = 0
 
-    def __init__(self, manufacturing, model, price, ink, type_eq="Printer", condition="new"):
-        super(Printer, self).__init__(type_eq, manufacturing, model, price)
+    def __init__(self, firm='Canon', ink='full'):
+        super(Printer, self).__init__(firm)
         self.ink = ink
-        self.condition = condition
-        self.__class__.count_printer += 1
 
     def action(self):
-        print("print documents")
+        print("Printing documents")
 
 
 class Scanner(OfficeEquipment):
 
-    def __init__(self, manufacturing, model, price, lamp, type_eq="Scanner", condition="new"):
-        super(Scanner, self).__init__(type_eq, manufacturing, model, price)
+    def __init__(self, firm='Canon', lamp='new'):
+        super(Scanner, self).__init__(firm)
         self.lamp = lamp
-        self.condition = condition
 
     def action(self):
-        print("scan documents")
+        print("Scanning documents")
 
 
 class Copier(OfficeEquipment):
-    def __init__(self, manufacturing, model, price, paper, type_eq="Copier", condition="new"):
-        super(Copier, self).__init__(type_eq, manufacturing, model, price)
+
+    def __init__(self, firm='Canon', paper='full'):
+        super(Copier, self).__init__(firm)
         self.paper = paper
-        self.condition = condition
 
     def action(self):
-        print("copy documents")
+        print("Coping documents")
 
 
-obj_1 = Printer("canon", "x1", 100, 1)
-obj_1.get_in_storage()
+storage1 = Storage("storage1", 10)
+storage2 = Storage("storage2", 10)
 
-for i in in_storage:
-    print(i.price, i.model)
+while True:
+    user_answer = input("What kind of equipment do you need? Printer, Scanner or Copier?: ").title()
+    if user_answer in [i.__name__ for i in OfficeEquipment.__subclasses__()]:
+        equipment = [i for i in OfficeEquipment.__subclasses__() if i.__name__ == user_answer][0]
+        break
+while True:
+    user_answer = input(f'What storage do you need? You can choose {[i.name for i in Storage.list_storage]}: ')
+    if user_answer in [i.name for i in Storage.list_storage]:
+        storage = [i for i in Storage.list_storage if i.name == user_answer][0]
+        break
+while True:
+    try:
+        user_answer = int(input(f'How mach {equipment.__name__}s du you need ?: '))
+        break
+    except ValueError as err:
+        print("Please enter the number only")
 
-# for _ in range(3):
-#     Printer("canon", "x1", 100, 1, 300)
-#
-# print(Printer.count_printer)
+for i in range(user_answer):
+    equipment1 = equipment()
+    storage.getting(equipment1)
 
-#print(obj_1.type_eq)
-# obj_1.action()
-
-
-# obj_2 = Scanner("Scanner", "hp", "205", 200, 1, 1)
-# print(obj_2.lamp)
-# obj_2.action()
-#
-# obj_3 = Copier("Copier", "xerox", "100", 150, 1, 100)
-# print(obj_3.paper)
-# obj_3.action()
-"""
-Пусть эта херня работает
-"""
+Storage.audit(storage)
